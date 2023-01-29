80aba6d2b51458e7802090191be60fb52fcfed7f
laurentDellaNegra <laurent.dellanegra@gmail.com>
2023-01-29 07:42:34 +0100

    ⚗️ Testing bundle size without solidquery

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,64 @@
-import { Component, createResource } from 'solid-js';
+// import {
+//   createQuery,
+//   QueryClient,
+//   QueryClientProvider,
+// } from "@tanstack/solid-query";
+import { Component, createResource, For, Match, Switch } from "solid-js";
 
-const fetchFakeData = async () => (await fetch('https://dummyjson.com/products/1')).json()
+// const queryClient = new QueryClient();
+
+const fetchFakeData = async () =>
+  (await fetch("https://dummyjson.com/products")).json();
+
+const Calculator: Component = () => {
+  const [data] = createResource(fetchFakeData);
+  return (
+    // <>
+    //   <span>{data.loading && "Loading..."}</span>
+    //   <For each={data()?.products}>
+    //     {(product) => <pre>{JSON.stringify(product, null, 2)}</pre>}
+    //   </For>
+    // </>
+    <Switch>
+      <Match when={data.loading}>
+        <p>Loading...</p>
+      </Match>
+      <Match when={data.error}>
+        <p>Error: {data.error.message}</p>
+      </Match>
+      <Match when={data.state === "ready"}>
+        <For each={data().products}>
+          {(product) => <pre>{JSON.stringify(product, null, 2)}</pre>}
+        </For>
+      </Match>
+    </Switch>
+  );
+};
+
+// const CalculatorSolidQuery: Component = () => {
+//   const query = createQuery(() => ["data"], fetchFakeData);
+//   return (
+//     <Switch>
+//       <Match when={query.isLoading}>
+//         <p>Loading...</p>
+//       </Match>
+//       <Match when={query.isError}>
+//         <p>Error: {(query.error as Error).message}</p>
+//       </Match>
+//       <Match when={query.isSuccess}>
+//         <For each={query.data.products}>
+//           {(product) => <pre>{JSON.stringify(product, null, 2)}</pre>}
+//         </For>
+//       </Match>
+//     </Switch>
+//   );
+// };
 
 const App: Component = () => {
-  const [data] = createResource(fetchFakeData)
   return (
-    <>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <pre>{JSON.stringify(data(), null, 2)}</pre>
-    </>
+    // <QueryClientProvider client={queryClient}>
+    <Calculator />
+    // </QueryClientProvider>
   );
 };
 
